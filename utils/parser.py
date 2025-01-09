61482123ded3760f4f6aaec1a4e48ef4fa4f9690
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2025-01-09 22:33:43 +0530

    bare bones

---
+++ utils/parser.py
@@ -0,0 +1,26 @@
+# This will hold the parser class logic for parsing a log
+
+import re
+from datetime import datetime
+import difflib
+
+class LogParser:
+    def extract_time(self,) -> str:
+        # this need to extract the time from the log
+        pass
+    
+    def extract_log_level(self,) -> str:
+        # this need to extract the log level from the log
+        pass
+    
+    def extract_log_message(self,) -> str:
+        # this need to extract the log message from the log
+        pass
+    
+    def extract_log_source(self,) -> str:
+        # this need to extract the log source from the log
+        pass
+    
+    def extract_log(self,) -> dict:
+        # this need to extract the log from the log and form a dictionary and return it
+        pass
